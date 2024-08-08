47f8246c690ca9a82bc1aeae1b9b8ebaebdd3afc
Teddyburgonde <bandama.teddy@gmail.com>
2024-08-08 09:35:59 +0200

    parsing elements ok but not refactoring

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 07:03:02 by tebandam          #+#    #+#             */
-/*   Updated: 2024/08/07 14:01:35 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/08/08 09:35:21 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,11 +58,23 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 int	main(int argc, char **argv)
 {
-
 	int	fd;
 	t_map *map;
 	char *line;
-
+	int	check_no;
+	int check_so;
+	int	check_we;
+	int	check_ea;
+	int	check_f;
+	int	check_c;
+	
+	check_no = 0;
+	check_so = 0;
+	check_we = 0;
+	check_ea = 0;
+	check_f = 0;
+	check_c = 0;
+	
 	map = NULL;
 	map = ft_calloc(1, sizeof(t_map));
 	if (!map)
@@ -83,36 +95,44 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	line = get_next_line(fd);
+	if (!line)
+	{
+		ft_putstr_fd("Error read\n", 2);
+		return (1);
+	}
 	while (line)
 	{
 		if (ft_strncmp(line, "NO", 2) == 0)
 		{
-			printf("'NO' is present.\n");
+			check_no++;
 		}
 		else if (ft_strncmp(line, "SO", 2) == 0)
 		{
-			printf("'SO' is present.\n");
+			check_so++;
 		}
 		else if (ft_strncmp(line, "WE", 2) == 0)
 		{
-			printf("'WE' is present.\n");
+			check_we++;
 		}
 		else if (ft_strncmp(line, "EA", 2) == 0)
 		{
-			printf("'EA' is present.\n");
+			check_ea++;
 		}
 		else if (ft_strncmp(line, "F", 1) == 0)
 		{
-			printf("'F' is present.\n");
+			check_f++;
 		}
 		else if (ft_strncmp(line, "C", 1) == 0)
 		{
-			printf("'C' is present.\n");
+			check_c++;
 		}
-		//ft_putstr_fd(line, 1);
 		free(line);
 		line = get_next_line(fd);
 	}
+	if (check_no != 1 || check_so != 1 || check_we != 1 || check_ea != 1 || check_f != 1 || check_c != 1)
+		ft_putstr_fd("Error parsing\n", 2);
+	else
+	 	ft_putstr_fd("Parsing ok all elements exist 😊\n", 1);
 	//! Penser a free ma structure t_map !
 	//? free_map(map);
 	close(fd);
