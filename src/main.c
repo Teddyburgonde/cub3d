9aed13461054538dbc202e7fd615045785ec7502
Teddyburgonde <bandama.teddy@gmail.com>
2024-08-15 00:29:09 +0200

    to do manage for color C and add EXIT_SUCCESS and EXIT_FAILURE

--- src/main.c
+++ src/main.c
@@ -6,46 +6,73 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 07:03:02 by tebandam          #+#    #+#             */
-/*   Updated: 2024/08/14 22:17:55 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/08/15 00:28:08 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 #include <stdio.h>
 
-int	ft_parse_map_elements_color(t_map *map)
+
+// Etudier , RGBA 
+// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+// {
+//     return (r << 24 | g << 16 | b << 8 | a);
+// }
+
+int	get_color(int *floor_color, char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (i < 3)
+	{
+		floor_color[i] = ft_atoi(tab[i]);
+		if (!floor_color[i] || floor_color[i] < 0 || floor_color[i] > 255)
+		{
+			printf("Error: Incorrect value\n");
+			ft_free(tab);
+			return (EXIT_FAILURE);
+		}
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	ft_parse_floor_color(char *map_line, int *floor_color)
 {
 	char	**tmp;
 	char	**tab;
+
+	tmp = ft_split(map_line, ' ');
+	if (!tmp)
+		return (EXIT_FAILURE);
+	tab = ft_split(tmp[1], ',');
+	if (!tab)
+	{
+		ft_free(tmp);
+		return (EXIT_FAILURE);
+	}
+	if (get_color(floor_color, tab) == 1)
+		return (EXIT_FAILURE);
+	ft_free(tmp);
+	ft_free(tab);
+	return (EXIT_SUCCESS);
+}
+
+int	ft_parse_map_elements_color(t_map *map)
+{
 	int		i;
 	map->floor_color = ft_calloc(3, sizeof(int));
+	if (!map->floor_color)
+		return (1);
 	i = 0;
-	tmp = NULL;
-	tab = NULL;
 	while (map->map[i])
 	{
 		if (ft_strncmp(map->map[i], "F ", 2) == 0)
 		{
-			tmp = ft_split(map->map[i], ' ');
-			tab = ft_split(tmp[1], ',');
-			map->floor_color[0] = ft_atoi(tab[0]);
-			map->floor_color[1] = ft_atoi(tab[1]);
-			map->floor_color[2] = ft_atoi(tab[2]);
-			if (map->floor_color[0] && map->floor_color[0] >= 0 && map->floor_color[0] <= 255
-			&& map->floor_color[1] && map->floor_color[1] >= 0 && map->floor_color[1] <= 255
-			&& map->floor_color[2] && map->floor_color[2] >= 0 && map->floor_color[2] <= 255)
-			{
-				printf("La valeur %d est correct\n", map->floor_color[0]);
-				printf("La valeur %d est correct\n", map->floor_color[1]);
-				printf("La valeur %d est correct\n", map->floor_color[2]);
-			}
-			else
-			{
-				printf("Valeur incorrect\n");
-				printf("La valeur map->floor_color[0] : %d \n", map->floor_color[0]);
-				printf("La valeur map->floor_color[1]:  %d \n", map->floor_color[1]);
-				printf("La valeur map->floor_color[2] : %d \n", map->floor_color[2]);
-			}
+			if (ft_parse_floor_color(map->map[i], map->floor_color) == 1)
+				return (1);
 		}
 		if (ft_strncmp(map->map[i], "C ", 2) == 0)
 		{
@@ -53,7 +80,7 @@ int	ft_parse_map_elements_color(t_map *map)
 		}
 		i++;
 	}
-	return (1);
+	return (EXIT_SUCCESS);
 }
 
 
@@ -63,14 +90,14 @@ int	main(int argc, char **argv)
 	t_texture	texture;
 
 	if (parsing_arguments(argc, argv) == 1)
-		return (1);
+		return (EXIT_FAILURE);
 	ft_memset(&map, 0, sizeof(t_map));
 	ft_memset(&texture, 0, sizeof(t_texture));
 	if (check_and_open_file(&map, argv) == 1)
-		return (1);
+		return (EXIT_FAILURE);
 	map.map = get_map(map.fd);
 	if (ft_parse_map_elements(&map) == 1)
-		return (1);
+		return (EXIT_FAILURE);
 	ft_parse_map_path_texture(&map, &texture);
 	ft_parse_map_elements_color(&map);
 
@@ -79,5 +106,5 @@ int	main(int argc, char **argv)
 	close(map.fd);
 	// //? free_map(map);
 	ft_putstr_fd("\033[32mEnd of program 😊\033[0m\n", 1);
-	return (0);
+	return (EXIT_SUCCESS);
 }
