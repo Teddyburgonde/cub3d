44bac751a46fb0c9fed1b0522c06d6e90c0ba9f5
Teddyburgonde <bandama.teddy@gmail.com>
2024-08-19 09:15:17 +0200

    added function for manage 0 and space in map

--- src/parsing_map.c
+++ src/parsing_map.c
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 16:23:56 by tebandam          #+#    #+#             */
-/*   Updated: 2024/08/18 18:16:36 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/08/19 09:12:21 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ static int	is_line_valid(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (str[i] != '1' 
+		if (str[i] != '1'
+			&& str[i] != '0'
+			&& str[i] != ' '
 			&& str[i] != 'N'
 			&&  str[i] != 'S'
 			&&  str[i] != 'E'
@@ -62,6 +64,8 @@ static int	is_top_and_bottom_wall_closed(char *str)
 	}
 	return (1);
 }
+// Accepter les 0        ok
+// Accepter les " "
 
 int	parse_map(t_map_data *map_data)
 {
