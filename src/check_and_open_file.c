ca5669d05b1ac6aec5c05a13958da518998da999
Teddyburgonde <bandama.teddy@gmail.com>
2024-09-12 07:18:37 +0200

    add close for it's a directory

--- src/check_and_open_file.c
+++ src/check_and_open_file.c
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 01:49:06 by tebandam          #+#    #+#             */
-/*   Updated: 2024/08/17 16:18:51 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/09/12 07:17:36 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int	check_and_open_file(t_map_data *map_data, char **argv)
 {
 	map_data->fd = open(argv[1], O_DIRECTORY);
 	if (map_data->fd > 0)
+	{
+		close(map_data->fd);
 		return (message_error_for_parsing_args("Is a directory.\n", 1));
-	close(map_data->fd);
+	}
 	map_data->fd = open(argv[1], O_RDONLY);
 	if (map_data->fd < 0 || map_data->fd > 1023)
 		return (message_error_for_parsing_args(
